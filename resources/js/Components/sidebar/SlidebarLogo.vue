8bab55ceb3a03f327291df37ae53ac695f08be61
Khang2411 <hk24112000@gmail.com>
2024-03-07 06:48:30 +0700

    update logo

--- resources/js/Components/sidebar/SlidebarLogo.vue
+++ resources/js/Components/sidebar/SlidebarLogo.vue
@@ -1,11 +1,16 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
 </script>
+
 <template>
     <div>
         <Link :href="route('dashboard')" class="text-center dark:text-white text-2xl">
-        <p>KZone</p>
+        <p
+            class="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-4xl font-black w-full">
+            KZone
+        </p>
         </Link>
     </div>
 </template>
+
 <style lang="scss" scoped></style>
